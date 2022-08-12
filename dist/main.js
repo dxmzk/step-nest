21391856fef4abde8ec5085bfe8d31061fb17d40
meng <meng@b-and-qchina.com>
2022-08-12 10:19:47 +0800

    添加请求限额

--- dist/main.js
+++ dist/main.js
@@ -3,13 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const platform_fastify_1 = require("@nestjs/platform-fastify");
 const compress_1 = require("@fastify/compress");
+const rate_limit_1 = require("@fastify/rate-limit");
 const platform_ws_1 = require("@nestjs/platform-ws");
 const app_module_1 = require("./app.module");
 const constants_1 = require("./config/constants");
 const log_interceptor_1 = require("./modules/interceptor/log_interceptor");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_fastify_1.FastifyAdapter());
-    app.register(compress_1.default, { encodings: ['gzip', 'deflate'] });
+    const fastAda = new platform_fastify_1.FastifyAdapter();
+    fastAda.register(rate_limit_1.default, { global: false, max: 30, timeWindow: 60000 });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, fastAda);
+    app.register(compress_1.default, { encodings: ["gzip", "deflate"] });
     app.useGlobalInterceptors(new log_interceptor_1.LogInterceptor());
     app.useWebSocketAdapter(new platform_ws_1.WsAdapter(app));
     await app.listen(constants_1.default.port);
--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,29 @@
  * Create Date: 2022-02
  * Desc: 应用程序入口文件
  */
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import fastifyCompress from '@fastify/compress';
-import fastifyRateLimit from '@fastify/rate-limit';
-import { WsAdapter } from '@nestjs/platform-ws';
-import { AppModule } from './app.module';
-import constants from './config/constants';
-import { LogInterceptor } from './modules/interceptor/log_interceptor';
+} from "@nestjs/platform-fastify";
+import fastifyCompress from "@fastify/compress";
+import rateLimit from "@fastify/rate-limit";
+import { WsAdapter } from "@nestjs/platform-ws";
+import { AppModule } from "./app.module";
+import constants from "./config/constants";
+import { LogInterceptor } from "./modules/interceptor/log_interceptor";
 // import AppExceptionFilter from './modules/exception/AppExceptionFilter';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const fastAda = new FastifyAdapter();
+  fastAda.register(rateLimit, { global: false, max: 30, timeWindow: 60000 }); // 请求限制，一分钟30次
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastAda);
   // app.useGlobalFilters(new AppExceptionFilter()); // 全局服务错误处理
   // app.useGlobalGuards(new RoleGuard()); // 全局角色守卫
-  app.register(fastifyCompress, { encodings: ['gzip', 'deflate'] }); // 压缩功能
+  app.register(fastifyCompress, { encodings: ["gzip", "deflate"] }); // 压缩功能
   app.useGlobalInterceptors(new LogInterceptor()); // 全局日志拦截器
   app.useWebSocketAdapter(new WsAdapter(app)); // WebSocket
-  // app.use(fastifyRateLimit()); // 为了保护您的应用程序免受暴力攻击，您必须实现某种速率限制
   await app.listen(constants.port);
 }
 
