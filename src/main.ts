60b3dd4eb5f52a13f7e9a174d21624cfd44a2eb6
meng <meng@b-and-qchina.com>
2022-08-27 18:15:44 +0800

    配置跨越问题

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,7 @@ async function bootstrap() {
   const fastAda = new FastifyAdapter();
   fastAda.register(rateLimit, { global: false, max: 5000, timeWindow: 60000 }); // 请求限制，一分钟5000次
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastAda);
+  app.enableCors(); // 开启跨源资源共享（CORS）请求资源
   app.register(fastifyMutipart, { addToBody: true }); // fastify文件上传支持库
   // app.useGlobalFilters(new AppExceptionFilter()); // 全局服务错误处理
   app.register(csrf); // 减轻跨站点请求伪造（CSRF 或 XSRF）一种恶意利用网站攻击
